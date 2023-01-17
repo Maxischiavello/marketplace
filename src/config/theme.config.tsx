0c00c32415f85d451e2c22e519906eff8c3a5272
Maxischiavello <maxischiavello12@gmail.com>
2023-01-17 14:25:27 -0300

    Notification component, notification context

--- src/config/theme.config.tsx
+++ src/config/theme.config.tsx
@@ -1,34 +1,55 @@
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import {
+    createTheme,
+    CssBaseline,
+    ThemeProvider
+} from "@mui/material"
 
 type ThemeProp = {
     children: JSX.Element
 }
 
 export enum themePalette {
-    BG = '#12181b',
-    LIME = '#C8FA5F'
+    BG = "#12181b",
+    LIME = "#C8FA5F",
+    // alert styles
+    ERROR_MAIN = "#f44335",
+    BG_ERROR_MAIN = "rgba(244,67,54,0.1",
 }
 
 const theme = createTheme({
-    palette: {
-        mode: 'dark',
-        background: {
+    palette:{
+        mode: "dark",
+        background:{
             default: themePalette.BG,
         },
         primary:{
             main: themePalette.LIME,
         },
     },
-    components: {
+    components:{
         MuiButton:{
-            defaultProps: {
+            defaultProps:{
                 style:{
-                    textTransform: 'none',
-                    boxShadow: 'none'
+                    textTransform: "none",
+                    boxShadow: "none",
                 }
             }
-        }
-    }
+        },
+        MuiAlert:{
+            defaultProps:{
+                style:{
+                    borderRadius: "0.8em",
+                    fontSize: "1em",
+                },
+            },
+            styleOverrides:{
+                standardError:{
+                    border: `1px solid ${themePalette.ERROR_MAIN}`,
+                    background: themePalette.BG_ERROR_MAIN,
+                }
+            }
+        },
+    },
 })
 
 export const ThemeConfig: React.FC<ThemeProp> = ({ children }) => {
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,16 @@
 import { Button, Container } from "@mui/material"
+import { useNotification } from "../../context/notification.context"
 
 export const HomePage: React.FC<{}> = () => {
+    const { getError } = useNotification()
+
+    const handleClick = () => {
+        getError("Tienes un error")
+    }
+
     return (
         <Container sx={{ mt: 9 }} maxWidth='xl'>
-            <Button fullWidth variant='contained'>
+            <Button fullWidth variant='contained' onClick={handleClick}>
                 Estamos en Home
             </Button>
         </Container>
